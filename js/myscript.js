5d28c8e6f4e60a5a1c7752cbd66f9b9684a908fb
dennis <dennis.olimpio0794@gmail.com>
2020-05-18 16:04:05 +0200

    add javascript

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,6 @@
 alert('La pagina potrebbe non visualizzarsi correttamente, sto imparando!');
 document.getElementById('my-playlist').innerHTML = 'Le tue raccolte';
 document.getElementById('news').innerHTML = 'Cosa aspetti? aggiorna!';
+document.getElementById('install').innerHTML = 'Prova la nostra app';
+
 document.getElementById('recently').innerHTML = 'Ascoltate di recente';
