edbe0c5bcb076f96a89df64080d78996891922ee
dennis <dennis.olimpio0794@gmail.com>
2020-05-18 15:38:32 +0200

    add javascript

---
+++ js/myscript.js
@@ -0,0 +1,4 @@
+alert('La pagina potrebbe non visualizzarsi correttamente, sto imparando!');
+document.getElementById('my-playlist').innerHTML = 'Le tue raccolte';
+document.getElementById('news').innerHTML = 'Cosa aspetti? aggiorna!';
+document.getElementById('recently').innerHTML = 'Ascoltate di recente';
